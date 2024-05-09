313b8e52d418fe9b39b246e83a04ec0475e04b6d
FateDGG <anghelandres12@gmail.com>
2024-05-09 09:29:45 -0500

    Imagenes Restaurantes

--- resources/views/components/restauranteCard.blade.php
+++ resources/views/components/restauranteCard.blade.php
@@ -11,7 +11,7 @@
 <div class="col top-pad">
     <a href="{{ url('/Restaurante') }}">
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('img/restaurantes.jpeg')}}" class="card-img-top" alt="...">
+        <img src="{{ asset( $restaurante->url_foto )}}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">{{ $restaurante->nombre }}</p> <!-- Mostrar el nombre del restaurante -->
             <!-- Puedes mostrar más información del restaurante aquí -->
