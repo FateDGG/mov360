2af5319ccaf7795fcee7ce5900a9cb068e53e924
FateDGG <anghelandres12@gmail.com>
2024-05-16 07:54:36 -0500

    Middleware creation

---
+++ app/Http/Middleware/RedirectIfNotAuthenticated.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfNotAuthenticated
+{
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return redirect('/')->with('error', 'Debes iniciar sesión para acceder a esta página');
+        }
+
+        return $next($request);
+    }
+
+}
