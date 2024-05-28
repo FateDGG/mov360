038dad03d87ef4100ae01bf33a2a42f41b32d5f4
JuanYepes511 <104527530+JuanYepes511@users.noreply.github.com>
2024-05-27 21:20:26 -0500

    AdminServicios

---
+++ resources/views/components/carListaTransporte.blade.php
@@ -0,0 +1,11 @@
+
+  <tr>
+    <td>2</td>
+    <td>María López</td>
+    <td>Carlos Ruiz</td>
+    <td>$30.00</td>
+    <td>
+      <button class="btn btn-info btn-sm">Ver Ubicación</button>
+      <button class="btn btn-danger btn-sm">Cancelar Servicio</button>
+    </td>
+  </tr>
